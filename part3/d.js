80780915c1c372249f4013c13af338ff0dd35edb
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-04-17 11:07:48 -0400

    Create d.js

---
+++ part3/d.js
@@ -0,0 +1,8 @@
+// nothing much here--
+
+
+// KEEP IN MIND::
+
+$ heroku config:set MONGODB_URI=mongodb+srv://fullstack:secretpasswordhere@cluster0-ostce.mongodb.net/note-app?retryWrites=true
+
+$ heroku config:set MONGODB_URI='mongodb+srv://fullstack:secretpasswordhere@cluster0-ostce.mongodb.net/note-app?retryWrites=true'
