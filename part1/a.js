58fcd6d79942d0d7cd4a11b9e5aa3bb3f645153b
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-04-17 10:03:58 -0400

    Update a.js

--- part1/a.js
+++ part1/a.js
@@ -1,1 +1,28 @@
-placeholder
+// Introduction to React
+
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// infile app.js
+const App = () => {
+  const name = 'Childerooni'
+  const age = 1139
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Rajan" age={5 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+
+// 
