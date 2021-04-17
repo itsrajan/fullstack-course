10e508519be51a4690afc922c33077a219966e39
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-04-17 11:03:47 -0400

    Create c.js

---
+++ part3/c.js
@@ -0,0 +1,22 @@
+// MONGO DB
+
+// MOSTLY JUST READING!!
+
+
+
+
+
+
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
